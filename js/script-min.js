7a2f000cac6b4ff83a2ff92e37f4e80504bfbe4d
Robert McAllister <robert@mcallisterdesign.com>
2017-01-24 00:31:15 -0700

    Javascript refinements

--- js/script-min.js
+++ js/script-min.js
@@ -1,1 +1,1 @@
-window.onload=function(){"use strict";document.getElementById("hD").className+=" hDEase",document.getElementById("hDTag").className+=" hDTagEase",document.getElementById("contactInfo").className+=" fadeIn"};
+var loadWindow=function(){"use strict";document.getElementById("hD").className+=" hDEase",document.getElementById("hDTag").className+=" hDTagEase",document.getElementById("contactInfo").className+=" fadeIn"};window.onload.loadWindow();
--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-window.onload = function () {
+var loadWindow = function () {
     "use strict";
     document.getElementById('hD').className += ' hDEase';
     document.getElementById('hDTag').className += ' hDTagEase';
     document.getElementById('contactInfo').className += ' fadeIn';
+
 };
+
+window.onload.loadWindow();
