c6514cd554430f2b4d0c4bc97e1adbabf926ca8b
Robert McAllister <robert@mcallisterdesign.com>
2017-01-23 22:23:51 -0700

    Refined typography and added CSS3 transitions

---
+++ js/script.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    "use strict";
+    document.getElementById('hD').className += ' hDEase';
+    document.getElementById('hDTag').className += ' hDTagEase';
+    document.getElementById('contactInfo').className += ' fadeIn';
+};
